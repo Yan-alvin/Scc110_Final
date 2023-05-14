7aad83c2018ad30274e5c44c740d2d2f4461a67b
Yan <yana@vdi-scclab-038.lancs.local>
2023-05-14 15:26:44 +0100

    added comments

--- final.c
+++ final.c
@@ -13,35 +13,48 @@ int main()
     file1 = fopen("test1.txt", "r");
 
     if (file1 != NULL) {
-
         char lineOfText1[1000];
         char dividedText1[1000][10];
         int i=0;
         int x=0;
 
+    //load all characters into the 2d array
 
-        while (fgets(lineOfText1, 1000, file1) != NULL) {
-            for(dx=lineOfText1;*dx; dx++){
-                if(isalpha(*dx)==0&&dividedText1[i][0]!=0){
-                    i++;
-                    x=0;
-                }
-                if(isalpha(*dx)!=0){
-                    *dx=tolower(*dx);
-                    dividedText1[i][x]=*dx;
-                    x++;
-                }
+    while (fgets(lineOfText1, 1000, file1) != NULL) {
+        for(dx=lineOfText1;*dx; dx++){
+            if(isalpha(*dx)==0&&dividedText1[i][0]!=0){
+                //checks if the variable is a character and try to skip those that aren't
+                i++;
+                x=0;
             }
-    }
-
-        while(q<i||dividedText1[q][b]!=0){
-            printf("%c",dividedText1[q][b]);
-            b++;
-            if(dividedText1[q][b]==0){
-               q++;
-               b=0;
+            if(isalpha(*dx)!=0){
+                *dx=tolower(*dx);
+                //put all characters to lower case
+                dividedText1[i][x]=*dx;
+                x++;
             }
         }
+    }
+    
+
+    while(q<i||dividedText1[q][b]!=0){
+        printf("%c",dividedText1[q][b]);
+        //print all to try to debug
+        b++;
+        if(dividedText1[q][b]==0){
+           q++;
+           b=0;
+        }
+    }
+
+    while(q<i||dividedText1[q][b]!=0){
+        printf("%c",dividedText1[q][b]);
+        b++;
+        if(dividedText1[q][b]==0){
+           q++;
+            b=0;
+        }
+    }
     
         fclose(file1);
     }
